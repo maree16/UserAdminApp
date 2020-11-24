9d5991189d65c3ee07ff4532a7ec4b2956a5f63a
maree16 <meight16meight@gmail.com>
2020-11-24 23:28:19 +0500

    Adding Http CLient

---
+++ ApiHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAdminApp
+{
+    class ApiHelper
+    {
+            public static HttpClient http { get; set; }
+
+
+        public static void InitializeClient()
+        {
+            http = new HttpClient();
+            http.DefaultRequestHeaders.Accept.Clear();
+            http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/JSON"));
+
+        }
+    }
+}
